407e067547e0a8db6a1a3bf567fdf9fe641cbc3d
714daniel <714.daniel.smith@gmail.com>
2015-11-20 11:55:55 -0800

    New proj

---
+++ lib/New.java
@@ -0,0 +1,27 @@
+package lib;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+i
+
+import distance.DistanceGetter;
+
+public class New {
+	public static void main(String[] args) throws FileNotFoundException {
+	File F = new File("H:\\My Documents\\reviews_Video_Games.json");
+	Scanner reviews = new Scanner(F);
+	
+	File Y = new File("newList.txt");
+	Scanner words = new Scanner(Y);
+	DistanceGetter dg = new DistanceGetter();
+}
+	public static void goThroughAmazon(Scanner reviews, Scanner words, DistanceGetter gd) {
+		
+	}
+	
+	
+	
+}
