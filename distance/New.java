dddfcef205a0b60cb05583c776e87cc73a8c378b
714daniel <714.daniel.smith@gmail.com>
2015-11-23 11:16:04 -0800

    Can't run

--- distance/New.java
+++ distance/New.java
@@ -8,17 +8,18 @@ import java.util.Scanner;
 
 public class New {
 	public static void main(String[] args) throws FileNotFoundException {
-	File F = new File("H:\\My Documents\\reviews_Video_Games.json");
-	Scanner reviews = new Scanner(F);
-	
-	File Y = new File("newList.txt");
-	Scanner words = new Scanner(Y);
-	DistanceGetter dg = new DistanceGetter();
-}
-	public static void goThroughAmazon(Scanner reviews, Scanner words, DistanceGetter gd) {
-		
+		File F = new File("H:\\My Documents\\reviews_Video_Games.json");
+		InputStream F1 = new InputStream(F);
+		Scanner reviews = new Scanner(F);
+
+		File Y = new File("newList.txt");
+		Scanner words = new Scanner(Y);
+		DistanceGetter dg = new DistanceGetter();
 	}
-	
-	
-	
+
+	public static void goThroughAmazon(Scanner reviews, Scanner words,
+			DistanceGetter gd) {
+
+	}
+
 }
